dce6221089bfe6fdf18b43b7f7c49b73f5aa6145
raviteja319 <ravitejarra@gmail.com>
2026-06-26 18:03:35 +0530

    full HTML and JS

--- jsbasic.js
+++ jsbasic.js
@@ -1,1 +1,18 @@
-let 
+<script>
+const otherCheckbox = document.getElementById("otherLanguage");
+const otherText = document.getElementById("otherText");
+
+otherCheckbox.addEventListener("change", function () {
+
+    if (this.checked) {
+        otherText.disabled = false;
+        otherText.required = true;
+        otherText.focus();
+    } else {
+        otherText.disabled = true;
+        otherText.required = false;
+        otherText.value = "";
+    }
+
+});
+</script>
