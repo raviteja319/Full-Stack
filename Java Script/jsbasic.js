84f6e7dc868ecc447baf0a71d8f3f20fc3ddf82c
raviteja319 <ravitejarra@gmail.com>
2026-07-24 14:33:59 +0530

    JS changes on basic and img url changes and git commands

---
+++ Java Script/jsbasic.js
@@ -0,0 +1,29 @@
+//the changes of js is done in console by inspecting 
+
+// to show pop up at opening of the tab
+// alert("wake up to reality"); 
+
+// math operators are directly added 2+3 and shows the result
+// 5
+
+// to change the complete text in the inter face HTML interface at and shows only hey
+// document.body.innerHTML = 'hey';
+
+//  JS follows BODAMS rule
+
+// also for adding percent % we do
+// for 1% = 1/100
+// 42.46 (10%) discount is given (10/100)
+// 42.46 * 0.10 
+// 4.246
+
+// Js functions diffrently for floats(decimal)
+// example: 0.3 + 0.5 results 0.800000000000004
+// to avoid this rounding
+
+// 20.95 + 7.99
+// 28.89999999999994 is the result 
+
+// Math.round(2095+799)/100
+// 28.84
+// rounding send to the nearest number 0 to 5 its preseeding and 6 to 9 its above number
